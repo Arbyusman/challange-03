0397236051faad49ed11e0ffd0be1b725e6f5168
arby usman <91175559+Arbyusman@users.noreply.github.com>
2022-09-05 00:37:02 +0800

    Update fungsi sort

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -1,15 +1,34 @@
 function sortCarByYearAscendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
-  console.log(cars);
+  // console.log(cars);
 
   // Clone array untuk menghindari side-effect
   // Apa itu side effect?
-  const result = [...cars];
+  // const result = [...cars];
 
   // Tulis code-mu disini
-  const ascendingByYear=result.sort((a,b) => a.year - b.year  );
-  console.log(ascendingByYear)
+  // const ascendingByYear=result.sort((a,b) => a.year - b.year  );
+  
+  function swap(arr, xp, yp) {
+    let temp = arr[xp];
+    arr[xp] = arr[yp];
+    arr[yp] = temp;
+  }
+
+  function bubbleSort(arr) {
+    let sort = arr;
+    let i, j;
+    for (i = 0; i < sort.length - 1; i++) {
+      for (j = 0; j < sort.length - i - 1; j++) {
+        if (sort[j].year > sort[j + 1].year) {
+          swap(sort, j, j + 1);
+        }
+      }
+    }
+    return sort;
+  }
+  let hasilsorting = bubbleSort(cars);
 
   // Rubah code ini dengan array hasil sorting secara ascending
-  return ascendingByYear;
+  return hasilsorting;
 }
